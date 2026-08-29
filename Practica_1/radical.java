5dc47b929ca7fdbadd3f75989b28ccc54520221a
Porto Emanuel <emanuelpora2007@gmail.com>
2026-08-29 00:01:08 -0600

    Add files via upload

---
+++ Practica_1/radical.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner open = new Scanner(System.in);
+		double radicando;
+		double answer; 
+		int root; 
+		
+		System.out.println("Pasame tu primer numero (radicando): ");
+		radicando = open.nextDouble();
+		System.out.print("pasame el segundo valor, o sea indice: ");
+		root = open.nextInt();
+		
+		answer = Math.pow(radicando, 1.0/root);
+		System.out.println("su valor de raiz calculado es: " + answer);
+		open.close();
+	}
+}
