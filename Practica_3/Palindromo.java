a462b8ae1704e74ce4fae6ed2e776dee2f9266bb
Diego Zayas <diegoraulzayas@gmail.com>
2026-09-11 00:11:01 -0600

    Añadir archivos guion_video.txt y Palindromo.java

---
+++ Practica_3/Palindromo.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Palindromo {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Ingresa un numero entero de 5 digitos:");
+        int num = sc.nextInt();
+        
+        
+        while (num < 10000 || num > 99999) {
+            System.out.println("Error: el numero debe tener 5 digitos");
+            System.out.println("Ingresa el nuevo numero:");
+            num = sc.nextInt();
+        }
+        
+      
+        if (esPalindromo(num)) {
+            System.out.println(num + " es un palindromo");
+        } else {
+            System.out.println(num + " no es un palindromo");
+        }
+        
+        sc.close();
+    }
+
+    
+    public static boolean esPalindromo(int num) {
+        
+        if ((num / 10000 == num % 10) && ((num / 1000) % 10 == (num / 10) % 10)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
